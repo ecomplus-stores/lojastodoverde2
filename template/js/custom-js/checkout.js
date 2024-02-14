700b99f7861679c48b7b6a0b5144dedab98eaafe
Vitor Rocha Gonçalves <35348281+vitorrgg@users.noreply.github.com>
2024-02-14 16:48:38 -0300

    Update checkout.js

--- template/js/custom-js/checkout.js
+++ template/js/custom-js/checkout.js
@@ -1,1 +1,3 @@
-
+document.body.innerHTML = document.body.innerHTML.replace('Destinatário', 'Nome do guia');
+document.body.innerHTML = document.body.innerHTML.replace('Complemento', 'Ônibus e estacionamento');
+document.body.innerHTML = document.body.innerHTML.replace('Referência', 'Data e hora');

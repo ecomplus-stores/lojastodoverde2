09f73bb7e3d364a78f0ad5ca3d09fd4f9415e571
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2024-02-14 16:36:20 -0300

    chore: remove code checkout

--- template/js/custom-js/checkout.js
+++ template/js/custom-js/checkout.js
@@ -1,3 +1,1 @@
-document.body.innerHTML = document.body.innerHTML.replace('Destinatário', 'Nome do guia');
-document.body.innerHTML = document.body.innerHTML.replace('Complemento ', 'Ônibus e estacionamento');
-document.body.innerHTML = document.body.innerHTML.replace('Referência  ', 'Data e hora');
+

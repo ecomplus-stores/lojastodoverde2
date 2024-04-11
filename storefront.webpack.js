4edaccb70221914cdec6477b7a741de3f806e7ca
matheusgnreis <matheus@e-com.club>
2024-04-10 22:35:57 -0300

    chore: create form to replace notes in checkout

--- storefront.webpack.js
+++ storefront.webpack.js
@@ -5,6 +5,8 @@ module.exports = () => ({
     alias: {
         './html/AddressForm.html': path.resolve(__dirname, 'template/js/custom-js/components/AddressForm.html'),
         './html/AddressForm.js': path.resolve(__dirname, 'template/js/custom-js/components/AddressForm.js'),
+        './html/EcCheckout.html': path.resolve(__dirname, 'template/js/custom-js/components/EcCheckout.html'),
+        './js/EcCheckout.js': path.resolve(__dirname, 'template/js/custom-js/components/EcCheckout.js'),
     }
   }
 })
